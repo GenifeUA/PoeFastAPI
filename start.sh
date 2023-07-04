f58613e21d833e01c8f4ddf10d02d40c6e6e82b0
Genife <GenifeUA@artemzheglov10@gmail.com>
2023-07-04 12:58:22 +0300

    Bump

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-uvicorn main:app --workers 4 --loop uvloop --host 0.0.0.0 --port 11000 --log-config "log.ini"
+uvicorn main:app --workers 4 --loop uvloop --host 127.0.0.1 --port 11000 --log-config "log.ini"
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 messages = [{'role': 'user', 'content': "Hi!"}]
 
-data = {"poe_model": "beaver", "token": "RyLV55NGcOXfSqnQ56svMg%3D%3D", "proxy": "",
+data = {"poe_model": "beaver", "token": "", "proxy": "",
         "messages": messages, "stream": False}
 
 response = requests.post("http://127.0.0.1:11000/poe/generation", json=data)
